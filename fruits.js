a245660ce46114d542b1d8b633ddaa30e93d855b
Athandwaclear <siyasangalakhiwe6@gmail.com>
2016-11-17 16:31:12 +0200

    I have commited file for the loops

---
+++ fruits.js
@@ -0,0 +1,11 @@
+
+var fruits = ['apple', 'pear', 'mango', 'orange', 'banana'];
+
+// to print out all the fruits in a list
+for (var i=0;i<fruits.length;i++){
+    console.log(fruits[i]);
+}
+
+// const assert = require('assert')
+//
+// assert.equal(fruits['apple', 'pear', 'mango', 'orange', 'banana']);
